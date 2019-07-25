00260495d74993c4c0d2707fd3dd0236dff910da
tanjoin <tanjoin@users.noreply.github.com>
2019-07-25 17:07:30 +0900

    Twitter を表示したときにホームが表示されてたら切り替える

---
+++ TwitterRealtimeTweet/scripts/content_script.js
@@ -0,0 +1,27 @@
+// content_script.js からリクエストを受け取り、アドレスバーにアイコンを表示する.
+((global) => {
+  'use strict';
+  chrome.extension.sendRequest({}, (res) => {});
+  setup();
+})(this.self || global);
+
+function setup() {
+  var target = document.querySelector('h2 > span');
+  if (!target) {
+    window.setTimeout(setup, 500);
+    return;
+  }
+  change();
+}
+
+function change() {
+  var target = document.querySelector('h2 > span');
+  if (target.innerText !== '最新ツイート') {
+    document.querySelector('h2 > span').parentElement.parentElement.parentElement.nextElementSibling.children[0].click();
+    window.setTimeout(menuClick, 1000);
+  }
+}
+
+function menuClick() {
+  document.querySelector('[role="menu"]').children[0].children[0].children[0].children[1].click();
+}
